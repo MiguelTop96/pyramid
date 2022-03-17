2156048428a49f92a52420c36e04530c22e6bfbc
MiguelTop96 <64411727+MiguelTop96@users.noreply.github.com>
2022-03-17 09:38:45 -0300

    Add files via upload

---
+++ desafio classico de python em c#/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class challenger{
+    static void Main(string[] arg){
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        Console.Write("insert a number to pyramid:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        for(int i = 1; i < number; i++){
+            Console.WriteLine(" ");
+
+            for(int j = i; j < (i * 2); j++){
+                Console.Write("*");
+            }
+        }
+
+
+        Console.ReadKey();
+    }
+}
